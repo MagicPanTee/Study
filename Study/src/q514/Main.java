332aeb2ad5a4c404288fbcbda8d83c93cd4799d0
s <s@DESKTOP-9HQ95S3>
2022-11-02 17:22:59 +0900

    Jungol

---
+++ Study/src/q514/Main.java
@@ -0,0 +1,22 @@
+package q514;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("height = ");
+		
+		String h = sc.nextLine();
+		
+		sc.close();
+		
+		System.out.println("Your height is " + h + "cm.");
+		
+		
+	}
+
+}
